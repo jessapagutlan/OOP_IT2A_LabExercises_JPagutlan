e112d9a6757c271ba2986c2e2f8a6ab3002870d0
jessapagutlan <jessapagutlan@gmail.com>
2025-09-12 13:14:38 +0800

    Add files via upload

---
+++ EXER2_PAGUTLAN/CarTester.java
@@ -0,0 +1,27 @@
+public class CarTester {
+    public static void main (String[]args){
+    Car c1 = new Car("Ford", "Yellow", "KAW12345", "AB123", "Gasoline", "Leather", "Touchscreen", true);
+    Car c2 = new Car("Toyota", "Red", "XYZ5678", "CD456", "Diesel", "Fabric", "Standard", true);
+    Car c3 = new Car("Honda", "Blue", "LMN9101", "EF789", "Hybrid", "Leather", "Touchscreen", true);
+    Car c4 = new Car("BMW", "Black", "QRS2345", "GH012", "Gasoline", "Alcantara", "Premium Sound", true);
+    Car c5 = new Car("Mitsubishi", "White", "TUV6789", "IJ345", "Electric", "Cloth", "Standard", false);
+    Car c6 = new Car("Chevrolet", "Silver", "CHE1234", "KL678", "Gasoline", "Leather", "Touchscreen", true);
+    Car c7 = new Car("Nissan", "Green", "NIS5678", "MN890", "Gasoline", "Fabric", "Standard", false);
+    Car c8 = new Car("Hyundai", "Orange", "HYU9101", "OP234", "Hybrid", "Leather", "Touchscreen", true);
+    Car c9 = new Car("Kia", "Brown", "KIA2345", "QR567", "Diesel", "Cloth", "Basic", false);
+    Car c10 = new Car("Mercedes", "Gray", "MER6789", "ST890", "Gasoline", "Leather", "Premium Sound", true);
+    
+
+    c1.displayInfo();
+    //c2.displayInfo();
+    // c2.displayInfo();
+    //c3.displayInfo();
+    //c4.displayInfo();
+    //c5.displayInfo();
+    //c6.displayInfo();
+    //c7.displayInfo();
+    //c8.displayInfo();
+    //c9.displayInfo();
+    //c10.displayInfo();
+}   
+}
